8873cd444b237ebc8bcb93ff9d1e2085b65ef2eb
gchacon <alcalamemo@gmail.com>
2023-07-31 22:39:27 +0100

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by guillermo chacon alcala on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
